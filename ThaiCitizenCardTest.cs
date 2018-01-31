f0db61adfda146cfcb16b15ebf54901a05dba166
iFew <chitpong@gmail.com>
2018-02-01 01:07:14 +0700

    Inistal Project and Problem

---
+++ ThaiCitizenCardTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ThaiCitizenCard
+{
+    public class ThaiCitizenCardTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
